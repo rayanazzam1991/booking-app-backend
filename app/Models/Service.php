cf942aba569b14f67e894dd0ee1415c649825709
rayanazzam <razzam@chip.com.lb>
2025-11-28 12:25:21 +0200

    Fix test case.

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    /** @use HasFactory<\Database\Factories\ServicesFactory> */
+    /** @use HasFactory<\Database\Factories\ServiceFactory> */
     use HasFactory;
 
     protected $guarded = [];
